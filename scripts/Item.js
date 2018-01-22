415b96b84831eed752248899dac1f8430a6bbd52
zakdrummond <zakdrummond@gmail.com>
2018-01-22 09:32:26 -0800

    item factory and validator

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,5 +1,27 @@
+'use strict';
+/* global cuid */
+
 const Item = (function () {
-  const foo = 'bar';
+  const validateName = function(name) {
+    if (!name) {
+      throw new Error('Name does not exist.');
+    }
+  };
   
-  return {};
-})
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false
+    };
+  };
+  
+  return {
+    validateName,
+    create
+  };
+}());
+
+
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,21 @@
 /* global shoppingList, cuid */
-
-
+'use strict';
 // eslint-disable-next-line no-unused-vars
 
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  const itemNames = [ '', 'apples', 'pears' ];
+  itemNames.forEach(name => {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  });
+  shoppingList.render();
 });
 
 
-// console.log(store);
-// console.log(foo);
-console.log(Item);
-console.log(foo);
