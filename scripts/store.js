9300f0b368bbf16854e22301d1555f2e857b37c6
Christina Moore <sayhitochristina@gmail.com>
2018-01-22 13:38:52 -0600

    update store to create, update, delete items

--- scripts/store.js
+++ scripts/store.js
@@ -11,11 +11,48 @@ const store = (function () {
   const hideCheckedItems = false;
   const searchTerm = '';
 
+  const findById = function(id) {
+    this.items.find(id);
+  }
+
+  const addItem = function(name) {
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch (e) {
+        console.log(`Cannot add item: ${e.message}`);
+    }
+  }
+
+  const findAndToggleCheck = function(id) {
+    const found = this.findById(id);
+    found.checked = !found.checked;
+  }
+
+  const findAndUpdateName = function(id, newName) {
+    try {
+      Item.validateName(newName);
+      findById(id).name = newName;
+    } catch (e) {
+      console.log(`Cannot update name: ${e.message}`);
+    }
+  }
+
+  const findAndDelete = function(id) {
+    this.items = this.items.filter(item => item.id != id);
+    console.log(this.items);
+  }
 
   return {
     items,
     hideCheckedItems,
     searchTerm,
+    findById,
+    addItem,
+    findAndToggleCheck,
+    findAndUpdateName,
+    findAndDelete,
+    
   };
-  
+
 }());
